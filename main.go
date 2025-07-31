7d5a67f9aab8aca6703800c5c83f99c4d412170e
thaivhm <hoangminhthaiv@gmail.com>
2025-07-31 20:19:02 +0700

    add: try register different handler functions
    plus: make a path that has trailing slash not accepting further items - /{$}

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippet box"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form to create new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	//log message to notify that the server is starting
 	log.Print("The server is running on port 4000")

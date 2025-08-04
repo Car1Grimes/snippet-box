d98e304cbba02b17962b5b5fa2137a38429c86ef
thaivhm <hoangminhthaiv@gmail.com>
2025-08-04 09:54:31 +0700

    apply helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,15 +13,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	files := []string{"./ui/html/base.tmpl.html", "./ui/html/pages/home.tmpl.html", "./ui/html/partials/nav.tmpl.html"}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.RequestURI)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 	}
 }
 
